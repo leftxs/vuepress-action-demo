d6db2da688e5ecc742fba1739bbe990e1a4e6151
lingmeixyz <56402933+lingmeixyz@users.noreply.github.com>
2019-11-16 15:01:06 +0800

    Minimum configuration

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,29 +2,10 @@ module.exports = {
     title: 'VuePress Demo',
     description: 'VuePress Demo',
     base: '/vuepress-action-demo/',
-    plugins: [ '@vuepress/active-header-links' ],
     themeConfig: {
-      // head: [
-      //   ['link', { rel: 'icon', href: '/logo.png' }]
-      // ],
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Action', link: '/action/' },
-        { text: 'Router', link: '/router/' },
-      ],
-      sidebar: [
-        {
-          title: 'Home',  
-          path: '/',     
-        },
-        {
-          title: 'Action',  
-          path: '/action/',     
-        },
-        {
-          title: 'Router',   
-          path: '/router/',  
-        },
-      ]
-    }
+        nav: [
+            { text: 'External', link: 'https://google.com', target:'_self', rel:'' },
+            { text: 'Guide', link: '/guide/', target:'_blank' }
+          ]
+        }
   }
